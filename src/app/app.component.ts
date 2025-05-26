413f8db2bbe486ef852c87cf59073f2b9f96e02e
Sufian Abu-Rab <s.aburab@gmail.com>
2025-05-26 10:43:11 +0200

    upgrade to angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['../demo-styling.css'],
+    standalone: false
 })
 export class AppComponent {
   title = 'angular-quickstart';
